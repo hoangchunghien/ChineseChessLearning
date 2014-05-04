6826ab319ea3378241e9b624bf7a6dbe66825fc3
Hiền Hoàng <hoangchunghien@gmail.com>
2014-05-04 14:56:52 +0700

    added exception folder

---
+++ WindowsPhone/IntelliCore/Core/Game/Player/Exceptions/PlayerJoinException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliCore.Core.Game.Player.Exceptions
+{
+    public class PlayerJoinException : Exception
+    {
+        public PlayerJoinException()
+            : base()
+        {
+
+        }
+
+        public PlayerJoinException(String message)
+            : base(message)
+        {
+
+        }
+    }
+}
---
+++ WindowsPhone/IntelliCore/Core/Game/Player/Notifies/PlayerReadyNotify.cs
@@ -0,0 +1,13 @@
+﻿using Intelli.Core.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliCore.Core.Game.Player.Notifies
+{
+    public class PlayerReadyNotify : INotify
+    {
+    }
+}
