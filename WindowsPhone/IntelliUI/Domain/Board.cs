53f5dfa6bd26fc36f5d38e3c808d1f1accc2b909
tieubinhbinh <luu.tn0609@sv.sgu.edu.vn>
2014-05-18 16:52:52 +0700

    add Domain

---
+++ WindowsPhone/IntelliUI/Domain/Board.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace IntelliUI.Domain
+{
+    public class Board
+    {
+        private Char[,] bArr;
+        
+        public Board()
+        {
+            initializeBoard();
+        }
+
+        private void initializeBoard()
+        {
+            this.bArr = new char[,]
+            {
+                {'r','k','m','a','g','a','m','k','r'},
+                {' ',' ',' ',' ',' ',' ',' ',' ',' '},
+                {' ','c',' ',' ',' ',' ',' ','c',' '},
+                {'p',' ','p',' ','p',' ','p',' ','p'},
+                {' ',' ',' ',' ',' ',' ',' ',' ',' '},
+
+                {' ',' ',' ',' ',' ',' ',' ',' ',' '},
+                {'P',' ','P',' ','P',' ','P',' ','P'},
+                {' ','C',' ',' ',' ',' ',' ','C',' '},
+                {' ',' ',' ',' ',' ',' ',' ',' ',' '},
+                {'R','K','M','A','G','A','M','K','R'}
+            };            
+        }
+
+        public Char[,] getBArr()
+        {
+            return this.bArr;
+        }
+
+        public void setBArr(char[,] bArr)
+        {
+            this.bArr = bArr;
+        }
+    }
+}
---
+++ WindowsPhone/IntelliUI/Domain/TouchItem.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using IntelliUI.Factory;
+
+namespace IntelliUI.Domain
+{
+    public partial class TouchItem : UserControl
+    {
+        private char p;
+
+        private Boolean visibleForTouch = true;
+        public int R { get; set; }
+        public int C { get; set; }
+
+        public TouchItem()
+        {
+            InitializeComponent();
+        }
+
+        public TouchItem(char ch)
+        {
+            InitializeComponent();
+            this.p = ch;
+
+            if (ch == 'G')
+                imgPiece.Source = ImageFactory.getImage('G');
+            if (ch == 'A')
+                imgPiece.Source = ImageFactory.getImage('A');
+            if (ch == 'M')
+                imgPiece.Source = ImageFactory.getImage('M');
+            if (ch == 'R')
+                imgPiece.Source = ImageFactory.getImage('R');
+            if (ch == 'C')
+                imgPiece.Source = ImageFactory.getImage('C');
+            if (ch == 'K')
+                imgPiece.Source = ImageFactory.getImage('K');
+            if (ch == 'P')
+                imgPiece.Source = ImageFactory.getImage('P');
+            if (ch == 'g')
+                imgPiece.Source = ImageFactory.getImage('g');
+            if (ch == 'a')
+                imgPiece.Source = ImageFactory.getImage('a');
+            if (ch == 'm')
+                imgPiece.Source = ImageFactory.getImage('m');
+            if (ch == 'r')
+                imgPiece.Source = ImageFactory.getImage('r');
+            if (ch == 'c')
+                imgPiece.Source = ImageFactory.getImage('c');
+            if (ch == 'k')
+                imgPiece.Source = ImageFactory.getImage('k');
+            if (ch == 'p')
+                imgPiece.Source = ImageFactory.getImage('p');
+
+        }
+
+        public void clearMask()
+        {
+            this.imgMask.Source = ImageFactory.getImage(' ');
+            
+        }
+
+        public void showMask()
+        {
+            this.imgMask.Source = ImageFactory.getImage('-');
+            this.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        public Boolean isVisibleForTouch()
+        {
+            return this.visibleForTouch;
+        }
+
+        public void setVisibleForTouch(bool flag)
+        {
+            this.visibleForTouch = flag;
+            if (flag)
+            {
+                this.showMask();
+            }
+            else
+                this.clearMask();
+        }
+
+        public char getP()
+        {
+            return this.p;
+        }
+
+    }
+}
